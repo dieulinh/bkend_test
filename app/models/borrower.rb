85053bf985e3659e528480215ff9d23cc7247c4f
Linh Nguyen <linhvn09@gmail.com>
2015-11-22 22:18:24 -0500

    fix wrong test result with property_types

--- app/models/borrower.rb
+++ app/models/borrower.rb
@@ -1,8 +1,9 @@
 class Borrower < ActiveRecord::Base
-  enum all_property_type: { single: 'single family', duplex: 'duplex', triplex: 'triplex', four_plex: '4-plex', condo: 'condo', others: 'others' }
+  enum all_property_type: ['single family', 'duplex', 'triplex', '4-plex', 'condo', 'others']
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
+  validates_presence_of :property_type
   validates_numericality_of :base_income, greater_than: 0
   validates_numericality_of :rental_income, greater_than_or_equal_to: 0
   validates_numericality_of :credit_score, less_than_or_equal_to: 620
@@ -10,10 +11,10 @@ class Borrower < ActiveRecord::Base
 
   def proper_type_should_be_in_allowed_list
     return if property_type.blank?
-    accepted_property_types = Borrower.all_property_types.tap {|hs| hs.delete('others')}
-    if accepted_property_types.include?('others')
-      errors.add(:property_type, 'Sorry, your subject property is not eligible.\
-       We only offer loan programs for residential 1-4 units at this time')
+    all_property_types = Borrower.all_property_types
+    accepted_property_types = all_property_types.except('others')
+    if property_type == Borrower.all_property_types['others']
+      errors.add(:property_type, 'Sorry, your subject property is not eligible. We only offer loan programs for residential 1-4 units at this time')
     end
   end
 end
--- spec/models/borrower_spec.rb
+++ spec/models/borrower_spec.rb
@@ -4,19 +4,20 @@ RSpec.describe Borrower, type: :model do
   describe "It should have valid data as a borrower" do
     let!(:borrower) { FactoryGirl.create(:borrower) }
     
-    it { should define_enum_for(:all_property_type).with({ single: 'single family', duplex: 'duplex', triplex: 'triplex', four_plex: '4-plex', condo: 'condo', others: 'others'}) }
+    it { should define_enum_for(:all_property_type).with(['single family', 'duplex', 'triplex', '4-plex', 'condo', 'others']) }
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
     it { should validate_presence_of :email }
+    it { should validate_presence_of :property_type }
     it { should validate_numericality_of(:base_income).is_greater_than(0) } 
     it { should validate_numericality_of(:rental_income).is_greater_than_or_equal_to(0) }
     it { should validate_numericality_of(:credit_score).is_less_than_or_equal_to(620) } 
     describe 'check if property_type in allowed list' do
-      let(:borrower) { FactoryGirl.build(:borrower, property_type: Borrower.others}
+      let(:borrower) { FactoryGirl.build(:borrower, property_type: Borrower.all_property_types['others']) }
+      
       it 'should return error message' do
         borrower.validate
-        expect(borrower.errors[:property_type]).to include("Sorry, your subject property is not eligible.\
-       We only offer loan programs for residential 1-4 units at this time")
+        expect(borrower.errors[:property_type]).to include('Sorry, your subject property is not eligible. We only offer loan programs for residential 1-4 units at this time')
       end
     end
   end
