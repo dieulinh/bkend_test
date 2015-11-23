fc8049a0bde55373fa6bf26b1e692fe6b14bf118
Linh Nguyen <linhvn09@gmail.com>
2015-11-23 00:18:02 -0500

    add more field to sample data for testing

--- spec/factories/borrower_factory.rb
+++ spec/factories/borrower_factory.rb
@@ -7,6 +7,11 @@ FactoryGirl.define do
     rental_income 389.00
     commission 345.00
     credit_score 480.00
+    mortage_payment 345.78
+    mortage_insurrance 12.77
+    homeowner_insurrance 69.12
+    property_tax 56.00
+    hoa_due 45.56
     property_type Borrower.all_property_types['4-plex']
   end
 end
